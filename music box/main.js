4294f7f40dabbf80da2390473c8ec8eb309e0db6
tangles2 <tangles10@aol.com>
2016-10-12 11:55:55 -0500

    Beautified my code

--- music box/main.js
+++ music box/main.js
@@ -1,37 +1,38 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var cNote = document.getElementById('cAudio');
-    $('#c').mousedown(function(){
+    $('#c').mousedown(function () {
         cNote.currentTime = 0;
         cNote.play();
     });
     var dNote = document.getElementById('dAudio');
-    $('#d').mousedown(function(){
+    $('#d').mousedown(function () {
         dNote.currentTime = 0;
         dNote.play();
     });
     var eNote = document.getElementById('eAudio');
-    $('#e').mousedown(function(){
+    $('#e').mousedown(function () {
         eNote.currentTime = 0;
         eNote.play();
     });
     var fNote = document.getElementById('fAudio');
-    $('#f').mousedown(function(){
+    $('#f').mousedown(function () {
         fNote.currentTime = 0;
         fNote.play();
     });
     var gNote = document.getElementById('gAudio');
-    $('#g').mousedown(function(){
+    $('#g').mousedown(function () {
         gNote.currentTime = 0;
         gNote.play();
     });
     var aNote = document.getElementById('aAudio');
-    $('#a').mousedown(function(){
+    $('#a').mousedown(function () {
         aNote.currentTime = 0;
         aNote.play();
     });
     var bNote = document.getElementById('bAudio');
-    $('#b').mousedown(function(){
+    $('#b').mousedown(function () {
         bNote.currentTime = 0;
         bNote.play();
     });
 });
+
